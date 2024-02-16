3d8b9716842825669e42983f7cbcb48a3a7b6e1d
Sasko <nekada89@gmail.com>
2024-02-16 13:22:36 +0100

    init + integrations(qwik, sitemap, unocss)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from 'unocss/astro';
+import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [UnoCSS({
+    injectReset: true // or a path to the reset file
+  }), sitemap(), qwikdev()]
 });
