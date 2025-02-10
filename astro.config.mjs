5c75f69b2bb0a9ea61e0ac1d61b80e6dbbc0d10b
Sasko <nekada89@gmail.com>
2025-02-10 21:37:20 +0100

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from 'unocss/astro';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: 'https://digibilder.se',
+
   integrations: [UnoCSS({
     injectReset: true
-  }), sitemap()]
+  }), sitemap()],
+
+  adapter: cloudflare()
 });
